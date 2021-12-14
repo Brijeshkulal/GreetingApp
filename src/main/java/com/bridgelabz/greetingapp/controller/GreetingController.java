95fbe6db587f55254d43e58165fe3cc9f50bc1dc
“Brijesh” <brijeshkulal5@gmail.com>
2021-12-14 19:29:48 +0530

    added service layer to get a message

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -2,6 +2,7 @@ package com.bridgelabz.greetingapp.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,9 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.greetingapp.model.Greeting;
 import com.bridgelabz.greetingapp.model.User;
+import com.bridgelabz.greetingapp.services.IGreetingService;
+
 
 @RestController
-@RequestMapping("/greeting")
 public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
@@ -38,6 +40,14 @@ public class GreetingController {
 	@PutMapping("put/{firstName}")
 	public Greeting greeting(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, firstName + " " + lastName));
+	}
+	
+	@Autowired
+	private IGreetingService greetingService;
+	
+	@GetMapping("greeting/service")
+	public Greeting greeting() {
+		return greetingService.greetingMessage();
 	}
 
 }
---
+++ src/main/java/com/bridgelabz/greetingapp/services/GreetingService.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.greetingapp.services;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import com.bridgelabz.greetingapp.model.Greeting;
+
+@Service
+public class GreetingService implements IGreetingService {
+    private static final String template = "Hello world";
+    private final AtomicLong counter = new AtomicLong();
+
+
+    @Override
+    public Greeting greetingMessage() {
+        return new Greeting(counter.incrementAndGet(), String.format(template));
+    }
+}
